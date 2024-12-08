8e9a07b2a1320ed2e8abf5984122dd5149adb11d
wunaitian <61442096+wunaitian@users.noreply.github.com>
2024-12-08 16:16:57 +0800

    Update image_loader.py

--- BH_LymphDS/custom/image_loader.py
+++ BH_LymphDS/custom/image_loader.py
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d28339c03359239d2e995013c3b41d4407b2d19ae881decc5168b7cf18787371
-size 1196
+import nibabel as nib
+import numpy as np
+from PIL import Image
+import SimpleITK as sitk
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        img = Image.open(f)
+        return img.convert('RGB')
+
+
+def nii_loader(path):
+    return np.array(nib.load(path).dataobj)
+
+def sitk_loader(path):
+    return sitk.GetArrayFromImage(sitk.ReadImage(path))
+
+
+
+def accimage_loader(path):
+    import accimage
+    try:
+        return accimage.Image(path)
+    except IOError:
+        # Potentially a decoding problem, fall back to PIL.Image
+        return pil_loader(path)
+
+
+def default_loader(path, nii_loader='nibabel'):
+    from torchvision import get_image_backend
+    if get_image_backend() == 'accimage':
+        return accimage_loader(path)
+    else:
+        if path.endswith('.nii.gz') or path.endswith('.nii'):
+            if nii_loader == 'nibabel':
+                return nii_loader(path)
+            else:
+                # logger.info('Using SimpleITK')
+                return sitk_loader(path)
+        else:
+            return pil_loader(path)
