a6ed95c7ecc4defa7c3dd562423d5104ec5afc04
wunaitian <61442096+wunaitian@users.noreply.github.com>
2024-12-08 16:18:34 +0800

    Update transformer_factory.py

--- BH_LymphDS/core/transformer_factory.py
+++ BH_LymphDS/core/transformer_factory.py
@@ -1,3 +1,36 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8bcdd371d6a84b79c7825e117c159954d3c852b317f4b9633eda3f2c57009ace
-size 1540
+from torchvision.transforms import transforms
+
+
+
+__all__ = ['create_standard_image_transformer']
+
+
+def create_standard_image_transformer(input_size, phase='train', normalize_method='imagenet',
+                                      **kwargs):
+    """Standard image transformer.
+
+    :param input_size: The core's input image size.
+    :param phase: phase of transformer, train or valid or test supported.
+    :param normalize_method: Normalize method, imagenet or -1+1 supported.
+    :param is_nii: 是不是多通过nii，当成2d来训练
+    :return:
+    """
+    assert phase in ['train', 'valid', 'test'], "`phase` not found, only 'train', 'valid', 'test' supported!"
+    normalize = {'imagenet': [[0.485, 0.456, 0.406], [0.229, 0.224, 0.225]],
+                 '-1+1': [[0.5, 0.5, 0.5], [0.5, 0.5, 0.5]]}
+    assert normalize_method in normalize, "`normalize_method` not found, only 'imagenet', '-1+1' supported!"
+    if phase == 'train':
+        return transforms.Compose([
+            # transforms.RandomResizedCrop(input_size),
+            transforms.Resize(input_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomErasing(),
+            transforms.ToTensor(),
+            transforms.Normalize(*normalize[normalize_method])])
+    else:
+        return transforms.Compose([
+            transforms.Resize(input_size),
+            # transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize(*normalize[normalize_method])])
+
